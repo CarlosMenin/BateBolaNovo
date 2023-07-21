6c8bedf9112eab8b1943791c0e6f31f3f683e386
CarlosMenin <carlosmenin0105@gmail.com>
2023-07-21 11:21:09 -0300

    Atualizado

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { BiSearch } from 'react-icons/bi';
+
+const Search = () => {
+    return (
+        <div className="
+            border-[1px]
+            w-full
+            md:w-auto
+            py-2
+            rounded-full
+            shadow-sm
+            hover:shadow-md
+            transition
+            cursor-pointer
+        ">
+            <div className="flex flex-row items-center justify-between">
+                <div className="text-sm font-semibold px-6">
+                    Qualquer Lugar
+                </div>
+                <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+                    Qualquer Semana
+                </div>
+                <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+                    <div className="hidden sm:block"> Adicionar Participantes</div>
+                    <div className="p-2 bg-purple-900 rounded-full text-white">
+                        <BiSearch size={18} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search
---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from './components/modals/RegisterModal';
+import Navbar from './components/navbar/Navbar';
+import './globals.css'
+import { Nunito } from 'next/font/google'
+import ToasterProvider from './providers/ToasterProvider';
+
+
+
+
+export const metadata = {
+  title: 'BateBola',
+  description: 'Marque eventos esportivos',
+}
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  )
+}
