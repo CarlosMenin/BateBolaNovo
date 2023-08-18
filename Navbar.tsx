ead5c8b540468ea1cd8cb9d296efecf387c5946d
ViniciusBAlves <89609677+ViniciusBAlves@users.noreply.github.com>
2023-08-18 16:38:41 -0300

    Add files via upload

---
+++ Navbar.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import Container from '../Container';
+import Categories from './Categories';
+import Logo from './Logo';
+import Search from './Search';
+import CleanFilter from './CleanFilter';
+import UserMenu from './UserMenu';
+import { SafeUser } from '@/app/types';
+import UserSearch from './UserSearch';
+import { useRouter } from 'next/navigation';
+
+interface NavbarProps{
+    currentUser?: SafeUser | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+    currentUser
+}) => {
+    const router = useRouter();
+    return (
+        <div className='fixed w-full bg-white z-10 shadow-sm'>
+            <div className='py-4 border-b-[1px]'>
+                <Container>
+                    <div className='
+                        flex
+                        flex-row
+                        items-center
+                        justify-between
+                        gap-3
+                        md:gap-0
+                    '>
+                        <Logo />
+                        <Search />
+                        <CleanFilter
+                            onClick={() => router.back}
+                        />
+                        <UserSearch />
+                        <UserMenu currentUser={currentUser} />
+                    </div>
+                </Container>
+            </div>
+            <Categories />
+        </div>
+    )
+}
+
+export default Navbar;
