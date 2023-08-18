880a6ea3f17a5a4fbf66690bdc1dda0a7dbfe7ba
arthurdossena <81803654+arthurdossena@users.noreply.github.com>
2023-08-17 23:48:37 -0300

    Update page.tsx
    Página de busca. Usuário não encontrado.

--- app/users/[name]/page.tsx
+++ app/users/[name]/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import getUsers from "@/app/actions/getUsers";
 
 interface IParams {
@@ -6,10 +7,33 @@ interface IParams {
 
 const UserPage = async ({ params }: { params: IParams }) => {
   const user = await getUsers(params);
+  
+  const frases = ["Da próxima vez, chute a bola mais forte!",
+                  "A bola caiu no vizinho... Tente de novo!",
+                  "Gol perdido. Tente novamente!",
+                  "Caiu no drible... Busque de novo!",
+                  "Chute novamente!",
+                  "Não desista em sua busca... Receba!",
+                  "O jogo ainda não acabou. Busque novamente!",
+                  "Enquanto não ouvir o apito, siga pesquisando!",
+                  "Segue o jogo!",
+                  "Lance limpo. Procure novamente!",
+                  "Siga na busca e faça o gol!"];
+  const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
 
   if (user === null) {
-    return <div>Bah</div>;
-}
+    return (
+      <div className="flex items-center justify-center h-screen flex-col">
+        <div className="text-xl font-semibold mb-4">
+          Nenhum usuário encontrado.
+        </div>
+        <img src="/images/Bola_centralizada.png" alt="Bola" className="mb-4 w-32" />
+        <div className="text-xl font-semibold mb-4">
+          {fraseAleatoria}
+        </div>
+      </div>
+    );
+  }
     
   return (<div>{user?.email}</div>);
 };
