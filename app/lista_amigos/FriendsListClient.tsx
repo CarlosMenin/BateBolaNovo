b24bf344c6d5357eee222d1ed402a2f0891199ab
arthurdossena <81803654+arthurdossena@users.noreply.github.com>
2023-08-23 23:06:26 -0300

    Update FriendsListClient.tsx
    A lista fica centralizada e exibe o email também

--- app/lista_amigos/FriendsListClient.tsx
+++ app/lista_amigos/FriendsListClient.tsx
@@ -10,14 +10,18 @@ interface FriendsListClientProps {
 
 const FriendsListClient: React.FC<FriendsListClientProps> = ({ friends, currentUser }) => {
     return (
-        <div className="mx-4"> {/* Adiciona margem em ambos os lados (esquerda e direita) */}
-            <h2 className="font-bold text-xl">Lista de Amigos</h2>
-            <ul>
+        <div className="flex flex-col items-center mx-auto w-1/3 mt-4">
+            <h2 className="font-bold text-xl mb-4">Lista de Amigos</h2>
+            <ul className="w-full">
                 {friends.map(friend => (
                     <li key={friend.id} className="mb-2">
                         <div className="flex items-center p-2 border rounded">
                             <Avatar src={friend?.image} />
-                            <span className="ml-2">{friend.name}</span>
+                            <div className="flex-grow ml-2 flex items-center justify-between">
+                                <span>{friend.name}</span>
+                                <div className="border-r border-gray-300 h-5 mx-2"></div>
+                                <span>{friend.email}</span>
+                            </div>
                         </div>
                     </li>
                 ))}
